7709127b669f19d86c2fac39fff2f3c3005e4ba8
SId02 <31335947+SId02@users.noreply.github.com>
2024-01-12 13:03:29 +0530

    add

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  data = [
+    { img: "https://source.unsplash.com/1600x900/?electronics", title: "Slide 1" },
+    { img: "https://source.unsplash.com/1600x900/?architecture", title: "Slide 2" },
+    { img: "https://source.unsplash.com/1600x900/?nature", title: "Slide 3" }
+  ];
 }
--- src/app/faqs/faqs.component.ts
+++ src/app/faqs/faqs.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import UsersJson from '../users.json';
+
+interface USERS {
+  id: Number;
+  name: String;
+  username: String;
+  email: String;
+}
 
 @Component({
   selector: 'app-faqs',
@@ -6,5 +14,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./faqs.component.css']
 })
 export class FaqsComponent {
+
+// Display data uisng json file
+  Users: USERS[] = UsersJson;
+  constructor(){
+    console.log(this.Users);
+  }
+
+// Counter app
+  public counter: number = 0;
+  public increment() {
+    this.counter++;
+    return this.counter;
+  }
+
+  public decrement() {
+    this.counter--;
+    return this.counter;
+  }
+
+  public reset() {
+    this.counter = 0;
+    return this.counter;
+  }
 
 }
